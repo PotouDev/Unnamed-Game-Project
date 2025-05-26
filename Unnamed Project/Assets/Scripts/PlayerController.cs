7ac61738b025ee7f69b2baec09a46f8cc9df3267
PotouDev <thatguypotou@gmail.com>
2025-05-26 02:09:45 -0400

    Completed basic movement & camera
    Added prototype textures as well.

--- Unnamed Project/Assets/Scripts/PlayerController.cs
+++ Unnamed Project/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,52 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     private Rigidbody rb;
+    public Transform cam;
+    private Vector3 movementDirection;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     void Update()
     {
-        handleMovement();
         handleJump();
+    }
+    private void FixedUpdate()
+    {
+        handleRotation();
+        handleMovement();
     }
     void handleMovement()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0, moveZ) * moveSpeed;
-        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+        // Get Input
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        // Get Camera Vectors
+        Vector3 cameraForward = cam.forward;
+        Vector3 cameraRight = cam.right;
+
+        // Flatten the vectors so the player doesn't move upward/downward
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        // Combine movement
+        movementDirection = (cameraForward * moveZ + cameraRight * moveX).normalized;
+
+        // Apply
+        rb.linearVelocity = movementDirection * moveSpeed + new Vector3(0, rb.linearVelocity.y, 0);
+    }
+    void handleRotation()
+    {
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
+        }
     }
     bool isGrounded()
     {
